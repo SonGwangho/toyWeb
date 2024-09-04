0f1c702349092fd14b9bf19ea778a380d12f75d0
손광호 <108635238+SonGwangho@users.noreply.github.com>
2024-09-04 14:38:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  Toastify(
-      Object.assign(
-        {
-          text: "테스트 토스트",
-        },
-        option
-      )
-    ).showToast();
-}
+  Toast.showToast("테스트");
+});
